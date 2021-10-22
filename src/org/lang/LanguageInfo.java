f51e5350a5a10021a2b6c3df4e870613b2faea6b
Hashimgithub31 <hashim.fshf31@gmail.com>
2021-10-22 20:25:56 +0530

    hasi

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,18 @@
+package org.lang;
+
+public class LanguageInfo {
+	
+	public void tamilLanguage() {
+		System.out.println("tamil language is spoken in tamilmadu");
+		
+	}
+	
+	public void englishLanguage() {
+		System.out.println("english most common language");
+	}
+	
+	public void hindiLanguage() {
+		System.out.println("hindi language is spoken in north");
+	}
+
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,25 @@
+package org.lang;
+
+public class StateDetails {
+	
+	public void southIndia() {
+		System.out.println("Tamilnadu, Andhra, Kerala, Karnataka");
+		
+	}
+	
+	public void northIndia() {
+		System.out.println("Bihar, up, mp, etc");
+	}
+	
+	public static void main(String[] args) {
+		StateDetails s=new StateDetails();
+		s.southIndia();
+		s.northIndia();
+		LanguageInfo l=new LanguageInfo();
+		l.tamilLanguage();
+		l.englishLanguage();
+		l.hindiLanguage();
+		
+	}
+
+}
